879e7d026ecab3a83713192170c77ac63852ea27
DESKTOP-6OVVB6Q\Askyr1m <akatokikwok@outlook.com>
2023-03-11 00:07:17 +0800

    [ver30, feat]: 创建Blaster人物蓝图.

---
+++ Source/Blaster/Character/BlasterCharacter.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BlasterCharacter.generated.h"
+
+/**
+ * BlasterCharacter, 项目Character基类
+ */
+UCLASS()
+class BLASTER_API ABlasterCharacter : public ACharacter
+{
+	GENERATED_BODY()
+public:
+	// Sets default values for this character's properties
+	ABlasterCharacter();
+public:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
