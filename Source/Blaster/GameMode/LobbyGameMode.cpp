49b5282c4796cb06a6e5baea9b8f271d21d813f3
DESKTOP-6OVVB6Q\Askyr1m <akatokikwok@outlook.com>
2023-03-13 21:55:50 +0800

    [ver32, arch]: 无缝传输和Lobby; 并配置过渡地图; 制作每张地图的GameMode.

---
+++ Source/Blaster/GameMode/LobbyGameMode.cpp
@@ -0,0 +1,22 @@
+﻿#include "LobbyGameMode.h"
+#include "GameFramework/GameStateBase.h"
+
+void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+
+	/** GameState is used to replicate game state relevant properties to all clients.
+	* 即GameState字段包含了一系列客户端的状态.可以访问到所有的客户端PS
+	*/
+	int32 NumberOfPlayers = GameState.Get()->PlayerArray.Num();
+	if (NumberOfPlayers == 2) {
+		UWorld* World = GetWorld();
+		if (World) {
+			// 启用无缝传输, 提升体验
+			bUseSeamlessTravel = true;
+
+			/** 使用UWorld::ServerTravel 把服务端无缝传送至一个新关卡; 同时所有链接至此服务器的所有客户端也会跟随传送 */
+			World->ServerTravel(FString("/Game/Maps/BlasterMap?listen"));
+		}
+	}
+}
---
+++ Source/Blaster/GameMode/LobbyGameMode.h
@@ -0,0 +1,19 @@
+﻿
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "LobbyGameMode.generated.h"
+
+/**
+ * Lobby的GM
+ * 继承自GameMode而非GameModeBase, 因此持有更多的功能
+ */
+UCLASS()
+class BLASTER_API ALobbyGameMode : public AGameMode
+{
+	GENERATED_BODY()
+public:
+	// 覆写PostLogin
+	virtual void PostLogin(APlayerController* NewPlayer) override;
+};
